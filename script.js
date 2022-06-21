ea2c3480880f23a4a078aa717df1d923c7c399f5
muak1 <muak1ayomide@gmail.com>
2022-06-21 20:25:57 +0100

    started the intro to js

---
+++ script.js
@@ -0,0 +1,18 @@
+var firstName = 'Muhiz'
+var lastName = 'Akanni'
+var height = '176m tall'
+var country = 'Nigeria'
+
+fullname = firstName + ' ' + lastName
+
+document.write("Name:" + ' ' + fullname + '<br/>')
+document.write("Height:" + ' ' + height + '<br/>')
+document.write("Country:" + ' ' + country + '<br/>')
+
+console.log("Name:" + ' ' + fullname)
+console.log("Height:" + ' ' + height)
+console.log("Country:" + ' ' + country)
+
+alert("Name:" + ' ' + fullname)
+alert("Height:" + ' ' + height)
+alert("Country:" + ' ' + country)
